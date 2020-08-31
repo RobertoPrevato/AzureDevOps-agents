0cb81b05464d8adeb0f737b4fc35bca881771249
Roberto Prevato <roberto.prevato@gmail.com>
2020-08-31 16:24:10 +0200

    Updates pypy version and download url :snake:
    Fixes Python agent build, broken by change in external resource (PyPy download URL).

--- ubuntu18.04-python/scripts/installers/python.sh
+++ ubuntu18.04-python/scripts/installers/python.sh
@@ -11,27 +11,27 @@ source $HELPER_SCRIPTS/document.sh
 # Install Python 3, pip3
 apt-get install -y --no-install-recommends python3 python3-dev python3-pip
 
-# Install PyPy 3.5 to $AGENT_TOOLSDIRECTORY
-wget -q -P /tmp https://bitbucket.org/pypy/pypy/downloads/pypy3.5-v7.0.0-linux64.tar.bz2
-tar -x -C /tmp -f /tmp/pypy3.5-v7.0.0-linux64.tar.bz2
-rm /tmp/pypy3.5-v7.0.0-linux64.tar.bz2
-mkdir -p $AGENT_TOOLSDIRECTORY/PyPy/3.5.3
-mv /tmp/pypy3.5-v7.0.0-linux64 $AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64
-touch $AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64.complete
+# Install PyPy 3.6 to $AGENT_TOOLSDIRECTORY
+wget -q -P /tmp https://downloads.python.org/pypy/pypy3.6-v7.3.1-linux64.tar.bz2
+tar -x -C /tmp -f /tmp/pypy3.6-v7.3.1-linux64.tar.bz2
+rm /tmp/pypy3.6-v7.3.1-linux64.tar.bz2
+mkdir -p $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7
+mv /tmp/pypy3.6-v7.3.1-linux64 $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64
+touch $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64.complete
 
 # add pypy3 to PATH by default
-ln -s $AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64/bin/pypy3 /usr/local/bin/pypy3
+ln -s $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64/bin/pypy3 /usr/local/bin/pypy3
 # pypy3 will be the python in PATH when its tools cache directory is prepended to PATH
 # PEP 394-style symlinking; don't bother with minor version
-ln -s $AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64/bin/pypy3 $AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64/bin/python3
-ln -s $AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64/bin/python3 $AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64/bin/python
+ln -s $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64/bin/pypy3 $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64/bin/python3
+ln -s $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64/bin/python3 $AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64/bin/python
 
 # Install latest Pip for PyPy3
-$AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64/bin/pypy3 -m ensurepip
-$AGENT_TOOLSDIRECTORY/PyPy/3.5.3/x64/bin/pypy3 -m pip install --ignore-installed pip
+$AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64/bin/pypy3 -m ensurepip
+$AGENT_TOOLSDIRECTORY/PyPy/3.6-v7/x64/bin/pypy3 -m pip install --ignore-installed pip
 
 # Run tests to determine that the software installed as expected
-# python pypy 
+# python pypy
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
 for cmd in python3 pypy3; do
     if ! command -v $cmd; then
